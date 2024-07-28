54f61017085ad67a04990c3a147d4d5ef8e4108c
apil <078bct017.apil@pcampus.edu.np>
2024-07-28 23:15:45 +0545

    fix: add header to debug image msg

--- yolov8_ros/yolov8_ros/debug_node.py
+++ yolov8_ros/yolov8_ros/debug_node.py
@@ -14,287 +14,309 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
-import cv2
 import random
-import numpy as np
 from typing import Tuple
 
-import rclpy
-from rclpy.duration import Duration
-from rclpy.qos import QoSProfile
-from rclpy.qos import QoSHistoryPolicy
-from rclpy.qos import QoSDurabilityPolicy
-from rclpy.qos import QoSReliabilityPolicy
-from rclpy.lifecycle import LifecycleNode
-from rclpy.lifecycle import TransitionCallbackReturn
-from rclpy.lifecycle import LifecycleState
-
+import cv2
 import message_filters
+import numpy as np
+import rclpy
 from cv_bridge import CvBridge
-from ultralytics.utils.plotting import Annotator, colors
-
+from rclpy.duration import Duration
+from rclpy.lifecycle import LifecycleNode, LifecycleState, TransitionCallbackReturn
+from rclpy.qos import (
+  QoSDurabilityPolicy,
+  QoSHistoryPolicy,
+  QoSProfile,
+  QoSReliabilityPolicy,
+)
 from sensor_msgs.msg import Image
-from visualization_msgs.msg import Marker
-from visualization_msgs.msg import MarkerArray
-from yolov8_msgs.msg import BoundingBox2D
-from yolov8_msgs.msg import KeyPoint2D
-from yolov8_msgs.msg import KeyPoint3D
-from yolov8_msgs.msg import Detection
-from yolov8_msgs.msg import DetectionArray
+from ultralytics.utils.plotting import Annotator, colors
+from visualization_msgs.msg import Marker, MarkerArray
+from yolov8_msgs.msg import (
+  BoundingBox2D,
+  Detection,
+  DetectionArray,
+  KeyPoint2D,
+  KeyPoint3D,
+)
 
 
 class DebugNode(LifecycleNode):
+  def __init__(self) -> None:
+    super().__init__("debug_node")
 
-    def __init__(self) -> None:
-        super().__init__("debug_node")
+    self._class_to_color = {}
+    self.cv_bridge = CvBridge()
 
-        self._class_to_color = {}
-        self.cv_bridge = CvBridge()
+    # params
+    self.declare_parameter("image_reliability", QoSReliabilityPolicy.BEST_EFFORT)
 
-        # params
-        self.declare_parameter("image_reliability",
-                               QoSReliabilityPolicy.BEST_EFFORT)
+    self.get_logger().info("Debug node created")
 
-        self.get_logger().info("Debug node created")
+  def on_configure(self, state: LifecycleState) -> TransitionCallbackReturn:
+    self.get_logger().info(f"Configuring {self.get_name()}")
 
-    def on_configure(self, state: LifecycleState) -> TransitionCallbackReturn:
-        self.get_logger().info(f"Configuring {self.get_name()}")
+    self.image_qos_profile = QoSProfile(
+      reliability=self.get_parameter("image_reliability")
+      .get_parameter_value()
+      .integer_value,
+      history=QoSHistoryPolicy.KEEP_LAST,
+      durability=QoSDurabilityPolicy.VOLATILE,
+      depth=1,
+    )
 
-        self.image_qos_profile = QoSProfile(
-            reliability=self.get_parameter(
-                "image_reliability").get_parameter_value().integer_value,
-            history=QoSHistoryPolicy.KEEP_LAST,
-            durability=QoSDurabilityPolicy.VOLATILE,
-            depth=1
+    # pubs
+    self._dbg_pub = self.create_publisher(Image, "dbg_image", 10)
+    self._bb_markers_pub = self.create_publisher(MarkerArray, "dgb_bb_markers", 10)
+    self._kp_markers_pub = self.create_publisher(MarkerArray, "dgb_kp_markers", 10)
+
+    return TransitionCallbackReturn.SUCCESS
+
+  def on_activate(self, state: LifecycleState) -> TransitionCallbackReturn:
+    self.get_logger().info(f"Activating {self.get_name()}")
+
+    # subs
+    self.image_sub = message_filters.Subscriber(
+      self, Image, "image_raw", qos_profile=self.image_qos_profile
+    )
+    self.detections_sub = message_filters.Subscriber(
+      self, DetectionArray, "detections", qos_profile=10
+    )
+
+    self._synchronizer = message_filters.ApproximateTimeSynchronizer(
+      (self.image_sub, self.detections_sub), 10, 0.5
+    )
+    self._synchronizer.registerCallback(self.detections_cb)
+
+    return TransitionCallbackReturn.SUCCESS
+
+  def on_deactivate(self, state: LifecycleState) -> TransitionCallbackReturn:
+    self.get_logger().info(f"Deactivating {self.get_name()}")
+
+    self.destroy_subscription(self.image_sub.sub)
+    self.destroy_subscription(self.detections_sub.sub)
+
+    del self._synchronizer
+
+    return TransitionCallbackReturn.SUCCESS
+
+  def on_cleanup(self, state: LifecycleState) -> TransitionCallbackReturn:
+    self.get_logger().info(f"Cleaning up {self.get_name()}")
+
+    self.destroy_publisher(self._dbg_pub)
+    self.destroy_publisher(self._bb_markers_pub)
+    self.destroy_publisher(self._kp_markers_pub)
+
+    return TransitionCallbackReturn.SUCCESS
+
+  def draw_box(
+    self, cv_image: np.array, detection: Detection, color: Tuple[int]
+  ) -> np.array:
+    # get detection info
+    label = detection.class_name
+    score = detection.score
+    box_msg: BoundingBox2D = detection.bbox
+    track_id = detection.id
+
+    min_pt = (
+      round(box_msg.center.position.x - box_msg.size.x / 2.0),
+      round(box_msg.center.position.y - box_msg.size.y / 2.0),
+    )
+    max_pt = (
+      round(box_msg.center.position.x + box_msg.size.x / 2.0),
+      round(box_msg.center.position.y + box_msg.size.y / 2.0),
+    )
+
+    # draw box
+    cv2.rectangle(cv_image, min_pt, max_pt, color, 2)
+
+    # write text
+    label = "{} ({}) ({:.3f})".format(label, str(track_id), score)
+    pos = (min_pt[0] + 5, min_pt[1] + 25)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(cv_image, label, pos, font, 1, color, 1, cv2.LINE_AA)
+
+    return cv_image
+
+  def draw_mask(
+    self, cv_image: np.array, detection: Detection, color: Tuple[int]
+  ) -> np.array:
+    mask_msg = detection.mask
+    mask_array = np.array([[int(ele.x), int(ele.y)] for ele in mask_msg.data])
+
+    if mask_msg.data:
+      layer = cv_image.copy()
+      layer = cv2.fillPoly(layer, pts=[mask_array], color=color)
+      cv2.addWeighted(cv_image, 0.4, layer, 0.6, 0, cv_image)
+      cv_image = cv2.polylines(
+        cv_image,
+        [mask_array],
+        isClosed=True,
+        color=color,
+        thickness=2,
+        lineType=cv2.LINE_AA,
+      )
+    return cv_image
+
+  def draw_keypoints(self, cv_image: np.array, detection: Detection) -> np.array:
+    keypoints_msg = detection.keypoints
+
+    ann = Annotator(cv_image)
+
+    kp: KeyPoint2D
+    for kp in keypoints_msg.data:
+      color_k = (
+        [int(x) for x in ann.kpt_color[kp.id - 1]]
+        if len(keypoints_msg.data) == 17
+        else colors(kp.id - 1)
+      )
+
+      cv2.circle(
+        cv_image,
+        (int(kp.point.x), int(kp.point.y)),
+        5,
+        color_k,
+        -1,
+        lineType=cv2.LINE_AA,
+      )
+
+    def get_pk_pose(kp_id: int) -> Tuple[int]:
+      for kp in keypoints_msg.data:
+        if kp.id == kp_id:
+          return (int(kp.point.x), int(kp.point.y))
+      return None
+
+    for i, sk in enumerate(ann.skeleton):
+      kp1_pos = get_pk_pose(sk[0])
+      kp2_pos = get_pk_pose(sk[1])
+
+      if kp1_pos is not None and kp2_pos is not None:
+        cv2.line(
+          cv_image,
+          kp1_pos,
+          kp2_pos,
+          [int(x) for x in ann.limb_color[i]],
+          thickness=2,
+          lineType=cv2.LINE_AA,
         )
 
-        # pubs
-        self._dbg_pub = self.create_publisher(Image, "dbg_image", 10)
-        self._bb_markers_pub = self.create_publisher(
-            MarkerArray, "dgb_bb_markers", 10)
-        self._kp_markers_pub = self.create_publisher(
-            MarkerArray, "dgb_kp_markers", 10)
+    return cv_image
 
-        return TransitionCallbackReturn.SUCCESS
+  def create_bb_marker(self, detection: Detection, color: Tuple[int]) -> Marker:
+    bbox3d = detection.bbox3d
 
-    def on_activate(self, state: LifecycleState) -> TransitionCallbackReturn:
-        self.get_logger().info(f"Activating {self.get_name()}")
+    marker = Marker()
+    marker.header.frame_id = bbox3d.frame_id
 
-        # subs
-        self.image_sub = message_filters.Subscriber(
-            self, Image, "image_raw", qos_profile=self.image_qos_profile)
-        self.detections_sub = message_filters.Subscriber(
-            self, DetectionArray, "detections", qos_profile=10)
+    marker.ns = "yolov8_3d"
+    marker.type = Marker.CUBE
+    marker.action = Marker.ADD
+    marker.frame_locked = False
 
-        self._synchronizer = message_filters.ApproximateTimeSynchronizer(
-            (self.image_sub, self.detections_sub), 10, 0.5)
-        self._synchronizer.registerCallback(self.detections_cb)
+    marker.pose.position.x = bbox3d.center.position.x
+    marker.pose.position.y = bbox3d.center.position.y
+    marker.pose.position.z = bbox3d.center.position.z
 
-        return TransitionCallbackReturn.SUCCESS
+    marker.pose.orientation.x = 0.0
+    marker.pose.orientation.y = 0.0
+    marker.pose.orientation.z = 0.0
+    marker.pose.orientation.w = 1.0
+    marker.scale.x = bbox3d.size.x
+    marker.scale.y = bbox3d.size.y
+    marker.scale.z = bbox3d.size.z
 
-    def on_deactivate(self, state: LifecycleState) -> TransitionCallbackReturn:
-        self.get_logger().info(f"Deactivating {self.get_name()}")
+    marker.color.b = color[0] / 255.0
+    marker.color.g = color[1] / 255.0
+    marker.color.r = color[2] / 255.0
+    marker.color.a = 0.4
 
-        self.destroy_subscription(self.image_sub.sub)
-        self.destroy_subscription(self.detections_sub.sub)
+    marker.lifetime = Duration(seconds=0.5).to_msg()
+    marker.text = detection.class_name
 
-        del self._synchronizer
+    return marker
 
-        return TransitionCallbackReturn.SUCCESS
+  def create_kp_marker(self, keypoint: KeyPoint3D) -> Marker:
+    marker = Marker()
 
-    def on_cleanup(self, state: LifecycleState) -> TransitionCallbackReturn:
-        self.get_logger().info(f"Cleaning up {self.get_name()}")
+    marker.ns = "yolov8_3d"
+    marker.type = Marker.SPHERE
+    marker.action = Marker.ADD
+    marker.frame_locked = False
 
-        self.destroy_publisher(self._dbg_pub)
-        self.destroy_publisher(self._bb_markers_pub)
-        self.destroy_publisher(self._kp_markers_pub)
+    marker.pose.position.x = keypoint.point.x
+    marker.pose.position.y = keypoint.point.y
+    marker.pose.position.z = keypoint.point.z
 
-        return TransitionCallbackReturn.SUCCESS
+    marker.pose.orientation.x = 0.0
+    marker.pose.orientation.y = 0.0
+    marker.pose.orientation.z = 0.0
+    marker.pose.orientation.w = 1.0
+    marker.scale.x = 0.05
+    marker.scale.y = 0.05
+    marker.scale.z = 0.05
 
-    def draw_box(self, cv_image: np.array, detection: Detection, color: Tuple[int]) -> np.array:
+    marker.color.b = keypoint.score * 255.0
+    marker.color.g = 0.0
+    marker.color.r = (1.0 - keypoint.score) * 255.0
+    marker.color.a = 0.4
 
-        # get detection info
-        label = detection.class_name
-        score = detection.score
-        box_msg: BoundingBox2D = detection.bbox
-        track_id = detection.id
+    marker.lifetime = Duration(seconds=0.5).to_msg()
+    marker.text = str(keypoint.id)
 
-        min_pt = (round(box_msg.center.position.x - box_msg.size.x / 2.0),
-                  round(box_msg.center.position.y - box_msg.size.y / 2.0))
-        max_pt = (round(box_msg.center.position.x + box_msg.size.x / 2.0),
-                  round(box_msg.center.position.y + box_msg.size.y / 2.0))
+    return marker
 
-        # draw box
-        cv2.rectangle(cv_image, min_pt, max_pt, color, 2)
+  def detections_cb(self, img_msg: Image, detection_msg: DetectionArray) -> None:
+    cv_image = self.cv_bridge.imgmsg_to_cv2(img_msg)
+    bb_marker_array = MarkerArray()
+    kp_marker_array = MarkerArray()
 
-        # write text
-        label = "{} ({}) ({:.3f})".format(label, str(track_id), score)
-        pos = (min_pt[0] + 5, min_pt[1] + 25)
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(cv_image, label, pos, font,
-                    1, color, 1, cv2.LINE_AA)
+    detection: Detection
+    for detection in detection_msg.detections:
+      # random color
+      label = detection.class_name
 
-        return cv_image
+      if label not in self._class_to_color:
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+        self._class_to_color[label] = (r, g, b)
 
-    def draw_mask(self, cv_image: np.array, detection: Detection, color: Tuple[int]) -> np.array:
+      color = self._class_to_color[label]
 
-        mask_msg = detection.mask
-        mask_array = np.array([[int(ele.x), int(ele.y)]
-                              for ele in mask_msg.data])
+      cv_image = self.draw_box(cv_image, detection, color)
+      cv_image = self.draw_mask(cv_image, detection, color)
+      cv_image = self.draw_keypoints(cv_image, detection)
 
-        if mask_msg.data:
-            layer = cv_image.copy()
-            layer = cv2.fillPoly(layer, pts=[mask_array], color=color)
-            cv2.addWeighted(cv_image, 0.4, layer, 0.6, 0, cv_image)
-            cv_image = cv2.polylines(cv_image, [mask_array], isClosed=True,
-                                     color=color, thickness=2, lineType=cv2.LINE_AA)
-        return cv_image
+      if detection.bbox3d.frame_id:
+        marker = self.create_bb_marker(detection, color)
+        marker.header.stamp = img_msg.header.stamp
+        marker.id = len(bb_marker_array.markers)
+        bb_marker_array.markers.append(marker)
 
-    def draw_keypoints(self, cv_image: np.array, detection: Detection) -> np.array:
+      if detection.keypoints3d.frame_id:
+        for kp in detection.keypoints3d.data:
+          marker = self.create_kp_marker(kp)
+          marker.header.frame_id = detection.keypoints3d.frame_id
+          marker.header.stamp = img_msg.header.stamp
+          marker.id = len(kp_marker_array.markers)
+          kp_marker_array.markers.append(marker)
 
-        keypoints_msg = detection.keypoints
-
-        ann = Annotator(cv_image)
-
-        kp: KeyPoint2D
-        for kp in keypoints_msg.data:
-            color_k = [int(x) for x in ann.kpt_color[kp.id - 1]
-                       ] if len(keypoints_msg.data) == 17 else colors(kp.id - 1)
-
-            cv2.circle(cv_image, (int(kp.point.x), int(kp.point.y)),
-                       5, color_k, -1, lineType=cv2.LINE_AA)
-
-        def get_pk_pose(kp_id: int) -> Tuple[int]:
-            for kp in keypoints_msg.data:
-                if kp.id == kp_id:
-                    return (int(kp.point.x), int(kp.point.y))
-            return None
-
-        for i, sk in enumerate(ann.skeleton):
-            kp1_pos = get_pk_pose(sk[0])
-            kp2_pos = get_pk_pose(sk[1])
-
-            if kp1_pos is not None and kp2_pos is not None:
-                cv2.line(cv_image, kp1_pos, kp2_pos, [
-                    int(x) for x in ann.limb_color[i]], thickness=2, lineType=cv2.LINE_AA)
-
-        return cv_image
-
-    def create_bb_marker(self, detection: Detection, color: Tuple[int]) -> Marker:
-
-        bbox3d = detection.bbox3d
-
-        marker = Marker()
-        marker.header.frame_id = bbox3d.frame_id
-
-        marker.ns = "yolov8_3d"
-        marker.type = Marker.CUBE
-        marker.action = Marker.ADD
-        marker.frame_locked = False
-
-        marker.pose.position.x = bbox3d.center.position.x
-        marker.pose.position.y = bbox3d.center.position.y
-        marker.pose.position.z = bbox3d.center.position.z
-
-        marker.pose.orientation.x = 0.0
-        marker.pose.orientation.y = 0.0
-        marker.pose.orientation.z = 0.0
-        marker.pose.orientation.w = 1.0
-        marker.scale.x = bbox3d.size.x
-        marker.scale.y = bbox3d.size.y
-        marker.scale.z = bbox3d.size.z
-
-        marker.color.b = color[0] / 255.0
-        marker.color.g = color[1] / 255.0
-        marker.color.r = color[2] / 255.0
-        marker.color.a = 0.4
-
-        marker.lifetime = Duration(seconds=0.5).to_msg()
-        marker.text = detection.class_name
-
-        return marker
-
-    def create_kp_marker(self, keypoint: KeyPoint3D) -> Marker:
-
-        marker = Marker()
-
-        marker.ns = "yolov8_3d"
-        marker.type = Marker.SPHERE
-        marker.action = Marker.ADD
-        marker.frame_locked = False
-
-        marker.pose.position.x = keypoint.point.x
-        marker.pose.position.y = keypoint.point.y
-        marker.pose.position.z = keypoint.point.z
-
-        marker.pose.orientation.x = 0.0
-        marker.pose.orientation.y = 0.0
-        marker.pose.orientation.z = 0.0
-        marker.pose.orientation.w = 1.0
-        marker.scale.x = 0.05
-        marker.scale.y = 0.05
-        marker.scale.z = 0.05
-
-        marker.color.b = keypoint.score * 255.0
-        marker.color.g = 0.0
-        marker.color.r = (1.0 - keypoint.score) * 255.0
-        marker.color.a = 0.4
-
-        marker.lifetime = Duration(seconds=0.5).to_msg()
-        marker.text = str(keypoint.id)
-
-        return marker
-
-    def detections_cb(self, img_msg: Image, detection_msg: DetectionArray) -> None:
-
-        cv_image = self.cv_bridge.imgmsg_to_cv2(img_msg)
-        bb_marker_array = MarkerArray()
-        kp_marker_array = MarkerArray()
-
-        detection: Detection
-        for detection in detection_msg.detections:
-
-            # random color
-            label = detection.class_name
-
-            if label not in self._class_to_color:
-                r = random.randint(0, 255)
-                g = random.randint(0, 255)
-                b = random.randint(0, 255)
-                self._class_to_color[label] = (r, g, b)
-
-            color = self._class_to_color[label]
-
-            cv_image = self.draw_box(cv_image, detection, color)
-            cv_image = self.draw_mask(cv_image, detection, color)
-            cv_image = self.draw_keypoints(cv_image, detection)
-
-            if detection.bbox3d.frame_id:
-                marker = self.create_bb_marker(detection, color)
-                marker.header.stamp = img_msg.header.stamp
-                marker.id = len(bb_marker_array.markers)
-                bb_marker_array.markers.append(marker)
-
-            if detection.keypoints3d.frame_id:
-                for kp in detection.keypoints3d.data:
-                    marker = self.create_kp_marker(kp)
-                    marker.header.frame_id = detection.keypoints3d.frame_id
-                    marker.header.stamp = img_msg.header.stamp
-                    marker.id = len(kp_marker_array.markers)
-                    kp_marker_array.markers.append(marker)
-
-        # publish dbg image
-        self._dbg_pub.publish(self.cv_bridge.cv2_to_imgmsg(cv_image,
-                                                           encoding=img_msg.encoding))
-        self._bb_markers_pub.publish(bb_marker_array)
-        self._kp_markers_pub.publish(kp_marker_array)
+    # publish dbg image
+    self._dbg_pub.publish(
+      self.cv_bridge.cv2_to_imgmsg(
+        cv_image, encoding=img_msg.encoding, header=img_msg.header
+      )
+    )
+    self._bb_markers_pub.publish(bb_marker_array)
+    self._kp_markers_pub.publish(kp_marker_array)
 
 
 def main():
-    rclpy.init()
-    node = DebugNode()
-    node.trigger_configure()
-    node.trigger_activate()
-    rclpy.spin(node)
-    node.destroy_node()
-    rclpy.shutdown()
+  rclpy.init()
+  node = DebugNode()
+  node.trigger_configure()
+  node.trigger_activate()
+  rclpy.spin(node)
+  node.destroy_node()
+  rclpy.shutdown()
